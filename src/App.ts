eb54199a406d0d1694e24f7d50c6fc43e3fad01c
Insu Byeon <yuki@yuki.kr>
2020-03-08 04:02:13 +0900

    add: debug module

--- src/App.ts
+++ src/App.ts
@@ -1,8 +1,11 @@
 import "./libs/Dotenv"
 import { WebApplication } from "./bin/www"
 
+import debug from "debug"
+
+const [logEvent, logWarning, logError] = [debug('www:event'), debug('www:warning'), debug('www:error')]
 const port = process.env.PORT || 3000
 
 WebApplication.listen(port, () =>
-  console.log(`Mabobsa Back-end API Application has started on port ${port}`)
+    logEvent(`Mabobsa Back-end API Application has started on port ${port}`)
 )
